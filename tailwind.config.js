7d47649f265ef4033487b1c61b0bfd5420f3308c
unrealtobi <tobiwunmie@gmail.com>
2024-02-26 12:09:42 +0100

    added hero section

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,js}",
+    "./index.html",
+    "./node_modules/tw-elements/dist/js/**/*.js",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require("tw-elements/dist/plugin.cjs")
+],
+  darkMode: 'class',
+}
+
